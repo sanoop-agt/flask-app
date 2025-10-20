92ce0b275c34aa9a6aeea88d380fd743afb011f1
agtsanoop <sanoopannangatt1994@gmail.com>
2025-10-20 13:06:43 +0000

    test

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+  message = "<h1><center> Hello World app! Version  2 {} <center><h1>".format(hostname)
+  return message
+
+if __name__ == "__main__":
+  
+  hostname = os.getenv('HOSTNAME',None)  
+  app.run(host='0.0.0.0',port=5000)
+  
